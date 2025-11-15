f4237854ba2e4eaeadc3af4cc3eb15579a547be0
soumyyaagrawal <soumyaagrawal2k6@gmail.com>
2025-11-15 16:08:59 +0530

    homepage

---
+++ app/home.jsx
@@ -0,0 +1,34 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-end justify-start bg-[url('/bg.svg')] bg-cover">
+      {/* New Wrapper Div for the text block */}
+      <div className="flex flex-col mb-36 ml-10 text-white"> 
+        
+        {/* Removed relative, mb-40, left-10 */}
+        <h1 className="text-6xl " style={{fontFamily: 'var(--font-geist)'}}>
+          From Nature's Best to Your Table
+        </h1>
+        
+        {/* Changed span to h3 for semantic meaning, removed relative, mb-32 */}
+        <h3 className="text-2xl mt-4" style={{fontFamily: 'var(--font-geist)'}}>
+          Purity, Nutrition, and Taste!
+        </h3>
+
+        <h3 className="text-xl mt-2 mt-4 max-w-xl" style={{fontFamily: 'var(--font-geist)'}}>
+         Unlocking creativity transforms farming. Explore techniques to enhance productivity and connect with the land.
+        </h3>
+
+        <div className="flex ">
+            <button className="mt-6 rounded-full bg-[#D0F348] px-6 py-3 text-lg font-semibold text-gray-900 align-middle">
+                Explore Products <img src="/arrow-right.svg" className="inline-block ml-1  "/>
+            </button>
+            <button className="mt-6 ml-4 rounded-full bg-white px-6 py-3 text-lg font-semibold text-black">
+                Our Story
+            </button>
+
+        </div>
+        
+      </div>
+    </div>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import React from "react";
+import Home from "./home.jsx";
+
+export default function Page() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
